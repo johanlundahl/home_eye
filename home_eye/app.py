3912655150b669021d11435415bb3992b9c76742
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-12-19 21:20:43 +0100

    main app module

---
+++ home_eye/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template ,redirect, url_for
+from home_eye.flask_app import FlaskApp
+from home_eye.model.user import User
+from home_eye import config
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from pytils import http
+
+app = FlaskApp(__name__)
+app.secret_key = config.app_secret_key
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    # load user from db    
+    return User('', '')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST' and all(x in request.form for x in ['username', 'password']):
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username == config.username and password == config.password:
+            login_user(User(username, password))
+            
+        return redirect(url_for('home'))
+    return 500
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/', methods=['GET'])
+@login_required
+def home():
+    code, json = http.get_json(config.latest_sensor_url)
+    print(json)
+    return render_template('home.html', sensor = json)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
