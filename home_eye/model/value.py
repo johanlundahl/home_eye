62cb9e41c5116fc6d98b02c1768d5c746f50e16f
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2020-05-19 06:53:10 +0200

    modified value to correct presentation

--- home_eye/model/value.py
+++ home_eye/model/value.py
@@ -20,27 +20,40 @@ class Value:
 		if isinstance(self._value, str):
 			return str(self._value.capitalize())
 		if isinstance(self._value, datetime):
-			return self._value.strftime('%-H:%M %-d %b')
+			return self._value.strftime('%-d %b %-H:%M')
 		return str(self._value)
 	
 	@property
 	def unit(self):
+		if self.isnumber() and self.need_unit_prefix():
+			factor, prefix = self.get_prefix()
+			return prefix + self._unit
 		return self._unit
 
+	def isnumber(self):
+		return isinstance(self._value, (int, float))
+
+	def need_unit_prefix(self):
+		return self._value >= 1000 and self._unit is not ''
+
+	def get_prefix(self):
+		if self._value >= 10**6:
+			return (10**6, 'M')
+		elif self._value >= 10**3:
+			return (10**3, 'k')
+		else:
+			return (1, '')
+
 	def format_number(self, value):
-		value = self.thousand(value)
+		if self.isnumber() and self.need_unit_prefix():
+			factor, prefix = self.get_prefix()
+			value = value / factor
 		value = self.round(value)
 		return value
 
 	def round(self, value):
 		return int(round(value, 0))
 
-	def thousand(self, value):
-		if value > 999 and self._unit is not '':
-			self._unit = 'k' + self._unit
-			return value/1000
-		else:
-			return value
-
 	def __str__(self):
 		return '{} {}'.format(self.display_value, self.unit)
+
