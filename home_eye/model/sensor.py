ff2bb93cb5cf5bfcd2da782345113af6d610e7af
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2020-05-19 06:51:40 +0200

    added status class with decoder

--- home_eye/model/sensor.py
+++ home_eye/model/sensor.py
@@ -42,6 +42,42 @@ class SensorDecoder:
         if 'humidity' in dct and 'temperature' in dct: 
             return Sensor(dct['name'], dct['temperature'], dct['humidity'], datetime.strptime(dct['timestamp'], '%Y-%m-%d %H:%M:%S'))
 
+class Status:
+    def __init__(self, count, size, oldest, newest):
+        self._count = Value(count)
+        self._size = Value(size, 'b')
+        self._oldest = Value(oldest)
+        self._newest = Value(newest)
+
+    @property
+    def count(self):
+        return self._count
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @property
+    def oldest(self):
+        return self._oldest
+    
+    @property
+    def newest(self):
+        return self._newest
+
+    def __repr__(self):
+        return 'Status({}, {}, {}, {})'.format(self._count, self._size, self._oldest, self._newest)
+
+    def __str__(self):
+        return 'Status({}, {}, {}, {})'.format(self._count, self._size, self._oldest, self._newest)        
+
+class StatusDecoder:
+    @classmethod
+    def decode(cls, dct):
+        if 'newest' in dct and 'oldest' in dct:
+            oldest = datetime.strptime(dct['oldest'], '%Y-%m-%d %H:%M:%S')
+            newest = datetime.strptime(dct['newest'], '%Y-%m-%d %H:%M:%S')            
+            return Status(dct['count'], dct['size'], oldest, newest)
 
 class SensorHistory:
     def __init__(self, sensors):
