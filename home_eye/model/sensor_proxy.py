d39f0c0187ace69dce8a691ba82600183781bcb2
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-01 21:16:34 +0100

    added calls for day and month history

--- home_eye/model/sensor_proxy.py
+++ home_eye/model/sensor_proxy.py
@@ -17,7 +17,17 @@ class SensorProxy:
 		status, trend = http.get('{}/api/sensors/{}/history?from={}&resolution={}'.format(self.base_url, name, from_day.strftime('%Y-%m-%d %H:%M:%S'), size))
 		sensors = json.loads(trend, object_hook=SensorDecoder.decode)
 		return SensorHistory(list(reversed(sensors)))
+	
+	def get_day(self, name, day):
+		status, trend = http.get('{}/api/v2/sensors/{}/history?date={}&page_size=1000'.format(self.base_url, name, day))
+		sensors = json.loads(trend, object_hook=SensorDecoder.decode)
+		return SensorHistory(list(reversed(sensors)))
 		
+	def get_days(self, name, first, last):
+		status, trend = http.get('{}/api/v2/sensors/{}/history?date[ge]={}&date[le]={}&page_size=1000'.format(self.base_url, name, first, last))
+		sensors = json.loads(trend, object_hook=SensorDecoder.decode)
+		return SensorHistory(list(reversed(sensors)))
+
 	def get_status(self):
 		status, summary = http.get('{}/api/status'.format(self.base_url))
 		return json.loads(summary, object_hook=StatusDecoder.decode)
