02c8a353305f3a2d61e3205c1e59514f3a6c8583
Johan Lundahl <johan.t.lundahl@gmail.com>
2021-01-02 10:07:18 +0100

    added decode for min and max

--- home_eye/model/sensor_proxy.py
+++ home_eye/model/sensor_proxy.py
@@ -12,6 +12,10 @@ class SensorProxy:
 		status, obj = http.get('{}/api/v2/sensors/{}/latest'.format(self.base_url, name))
 		return json.loads(obj, object_hook=SensorDecoder.decode)
 
+	def get_min_max(self, name, date):
+		status, obj = http.get('{}/api/v2/sensors/{}/min-max?date={}'.format(self.base_url, name, date))
+		return json.loads(obj, object_hook=SensorDecoder.decode)
+
 	def get_history(self, name, days, size):
 		from_day = datetime.now() - timedelta(days=days)
 		status, trend = http.get('{}/api/v2/sensors/{}/readings?from={}&resolution={}'.format(self.base_url, name, from_day.strftime('%Y-%m-%d %H:%M:%S'), size))
