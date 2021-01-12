13b13ee35367adf4675cb595fa3361523df93cf6
Johan Lundahl <johan.t.lundahl@gmail.com>
2021-01-12 19:32:37 +0100

    added history from and to

--- home_eye/model/solar_proxy.py
+++ home_eye/model/solar_proxy.py
@@ -13,10 +13,12 @@ class SolarProxy:
         status, objs = http.get_json('{}overview?api_key={}'.format(self.base_url, self.api_key))
         return Solar.from_dict(objs)
 
-    def get_energy_history(self, days):
+    def get_energy_latest(self, days):
         start_date = (datetime.now()-timedelta(days=days)).strftime('%Y-%m-%d')
         end_date = datetime.now().strftime('%Y-%m-%d')
+        return self.get_energy_history(start_date, end_date)
+
+    def get_energy_history(self, start_date, end_date):
         url = '{}energy?api_key={}&timeUnit=DAY&endDate={}&startDate={}'.format(self.base_url, self.api_key, end_date, start_date)
         status, objs = http.get_json(url)
         return SolarHistory.from_dict(objs)
-
